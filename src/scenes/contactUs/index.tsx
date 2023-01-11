68673d59b8dc4bd12ad873efa235a8a44090a052
Piotr Purzycki <piotr@purzycki.pl>
2023-01-11 22:27:55 +0100

    contact image removed

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
-import ContactUsJadzia from "@/assets/jadzia-malowana.png";
+
 import HText from "@/shared/HText";
 
 type Props = {
@@ -130,7 +130,7 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                 SUBMIT
               </button>
             </form>
-            <img src={ContactUsJadzia} />
+
           </motion.div>
 
           <motion.div
