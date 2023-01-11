5edca3e8ed7110d84938f8e32059a85a78ce8014
Piotr Purzycki <piotr@purzycki.pl>
2023-01-11 14:48:40 +0100

    final web

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-function App() {
-  return (
-    <div className="app">app
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import MyActivities from "@/scenes/myActivities";
+import Benefit from "@/scenes/benefit";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefit setSelectedPage={setSelectedPage} />
+      <MyActivities setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,102 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/jadzia1.png";
+import SponsorRedBull from "@/assets/ns.png";
+import SponsorForbes from "@/assets/ns2.png";
+import SponsorFortune from "@/assets/ns3.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE AND MAIN HEADER */}
+      <motion.div
+        className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6"
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+      >
+        {/* MAIN HEADER */}
+        <div className="z-10 mt-32 md:basis-3/5">
+          {/* HEADINGS */}
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative">
+              <div className="before:absolute before:-top-40 before:-left-20 before:z-[-1] md:before:content-['JADZIA'] md:before:text-[200px] md:before:opacity-5">
+                <h1 className="text-[100px] font-bold">JADZIA</h1>
+                <h2 className="text-[60px] font-bold">I'm a super dog!</h2>
+              </div>
+            </div>
+
+            <p className="mt-8 text-sm">
+              I was adopted and now i live a happy life. <br />
+              If you want to take me for a walk just write me an email, but i cant read.
+            </p>
+          </motion.div>
+
+          {/* ACTIONS */}
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Take me for a walk
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+            </AnchorLink>
+          </motion.div>
+        </div>
+
+        {/* IMAGE */}
+        <div
+          className="flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end"
+        >
+          <img alt="home-pageGraphic" src={HomePageGraphic} />
+        </div>
+      </motion.div>
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img className="h-10" alt="redbull-sponsor" src={SponsorRedBull} />
+              <img className="h-10" alt="forbes-sponsor" src={SponsorForbes} />
+              <img className="h-10" alt="fortune-sponsor" src={SponsorFortune} />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;
